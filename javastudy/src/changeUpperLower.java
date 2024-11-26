e9946387fe9f7ff81901a1ea51e3da4ce29a6ffd
hjimin027 <hjimin027@gmail.com>
2024-11-26 12:41:28 +0900

    변수 수정

--- javastudy/src/changeUpperLower.java
+++ javastudy/src/changeUpperLower.java
@@ -8,8 +8,8 @@ public class changeUpperLower {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        char b;
         
+        // char b;
         // for (int i=0; i<a.length(); i++){
         //     b = a.charAt(i);
         //     if(Character.isUpperCase(b)){
