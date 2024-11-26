b38690e9e88715c5555c887a3bd9708de9d8f4d7
hjimin027 <hjimin027@gmail.com>
2024-11-26 12:38:45 +0900

    생성

---
+++ javastudy/src/changeUpperLower.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181949
+// 문자열을 입력받아 대문자는 소문자로, 소문자는 대문자로 변환하여 출력하는 프로그램
+
+
+public class changeUpperLower {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        char b;
+        
+        // for (int i=0; i<a.length(); i++){
+        //     b = a.charAt(i);
+        //     if(Character.isUpperCase(b)){
+        //         System.out.print(Character.toLowerCase(b));
+        //     } else {
+        //         System.out.print(Character.toUpperCase(b));
+        //     }
+        // }
+
+        for (char i : a.toCharArray()) {
+            if(Character.isUpperCase(i)){
+                System.out.print(Character.toLowerCase(i));
+            } else {
+                System.out.print(Character.toUpperCase(i));
+            }
+        }
+    }
+}
--- javastudy/src/spinString.java
+++ javastudy/src/spinString.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
-public class turn_string {
+// https://school.programmers.co.kr/learn/courses/30/lessons/181945#
+// 문자열을 입력받아 90도 회전하여 출력하는 프로그램
+
+public class spinString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
